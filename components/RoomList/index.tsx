f4767b92497f16f982243c5fb03db48464fbac1c
HanYooJa <hyj03250@naver.com>
2024-04-13 15:46:34 +0900

    3

--- components/RoomList/index.tsx
+++ components/RoomList/index.tsx
@@ -1,11 +1,20 @@
-/*eslint-disable-@next/next/no-img-element */
 import { RoomType } from "@/interface"
 import { ReactNode } from "react"
+
+import Image from "next/image"
+import { BLUR_DATA_URL } from "@/constants"
 
 export function RoomItem({ room }: { room: RoomType }) {
   return (
     <div key={room.id}>
-        <img src={room?.images?.[0]} alt={room.title} className="rounded-md w-full h-auto object-fit"/>
+        <Image 
+        src={room?.images?.[0]}
+        alt={room.title}
+        width={500}
+        height={500}
+        placeholder="blur"
+        blurDataURL={BLUR_DATA_URL}
+        className="rounded-md w-full h-auto object-fit"/>
         <div className="mt-2 font-semibold text-sm">{room.title}</div>
         <span className="text-xs px-2 py-1 rounded-full bg-black text-white mt-1">{room.category}</span>
         <div className="mt-1 text-gray-400 text-sm">{room.address}</div>
